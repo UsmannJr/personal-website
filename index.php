fb6794f3775a4ead8bfe58db316a90f65e9e2ca3
Usman <usmanjr100@gmail.com>
2024-01-17 23:04:47 +0700

    Menambahkan paragraf didalam file index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad distinctio, atque fugiat aperiam facilis voluptatibus velit saepe, optio mollitia dicta quasi ex placeat magnam exercitationem! Fugit libero labore debitis praesentium.</p>
 </body>
 </html>
